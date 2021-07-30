51845d824de625a18f007c85fe7ef3c1e63abcd1
Andrew Wooldridge <triptych@gmail.com>
2021-07-29 19:38:29 -0700

    fixing newsletter

--- src/routes/newsletter.svelte
+++ src/routes/newsletter.svelte
@@ -1,15 +1,55 @@
 <div class="newsletter">
-    <p>Subscribe to my newsletter</p>
 
+    <div class="columns">
+        <div class="column is-half">
+            <figure class="image is-3by2">
+                <img src="/flowers.jpg" alt="flowers" class="newsletter-flower"/>              
+            </figure>           
+        </div>
+        <div class="column">
+            <p class="newsletter-blurb">
+                <b>I'm so glad you're here!</b> When you sign up for my newsletter, 
+                you not only will receive a <b>free gift</b> - a recipe from my 
+                upcoming book - you'll also be on the inside track to find 
+                out first about contests, specials, and giveaways. 
+                There might even be a <b>mystery</b> or two with a clue for you! 
+                Enter your email address below for my very best content. 
+                I believe Readers are friends I haven't met yet, so rest 
+                assured I'll never take up too much of your time and I'll 
+                make it fun and worthwhile. Live Life Illuminated! </p>
+            <form method="post" action="https://sendfox.com/form/1gjwk0/36j46j" class="sendfox-form" id="36j46j" data-async="true" data-recaptcha="true">
+                <p><input 
+                    type="email" 
+                    class="input"
+                    placeholder="Enter your email address" 
+                    name="email" required /></p>
+                <!-- no botz please -->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="a_password" tabindex="-1" value="" autocomplete="off" /></div>
+                <p><button 
+                    class="button is-primary"
+                    type="submit">Subscribe</button></p>
+                </form>
+                <script src="https://sendfox.com/js/form.js"></script>
+        </div>
 
-
-    <form method="post" action="https://sendfox.com/form/1gjwk0/36j46j" class="sendfox-form" id="36j46j" data-async="true" data-recaptcha="true">
-        <p><input type="email" placeholder="Email" name="email" required /></p>
-        <!-- no botz please -->
-        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="a_password" tabindex="-1" value="" autocomplete="off" /></div>
-        <p><button type="submit">Subscribe</button></p>
-        </form>
-        <script src="https://sendfox.com/js/form.js"></script>
-                    
+      </div>
                  
 </div>
+
+<style lang="scss">
+    .newsletter-flower {
+        margin: 12px;
+        border-radius: 4px;
+    }
+    .newsletter-blurb {
+        margin: 12px;
+    }
+    .newsletter {
+        form {
+            margin: 12px;
+        }
+        button.is-primary {
+            margin: 4px 0;
+        }
+    }
+</style>
