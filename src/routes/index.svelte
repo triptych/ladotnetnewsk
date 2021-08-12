221eb74b84fc2c932f0cbf72f4a3fc1d55d78368
Andrew Wooldridge <triptych@gmail.com>
2021-08-11 20:31:07 -0700

    Updating website

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,21 @@
 <div class="la-index">
 	<div class="la-lli">
 		<img src="/lli800x600.png" alt="live life illuminated"/><br/>
-		<img src="/home/LIsa-Anne-Just-released1-01.jpg" alt="live life illuminated"/>
+		<!--<img src="/home/LIsa-Anne-Just-released1-01.jpg" alt="live life illuminated"/>-->
 	</div>
-	
+	<div class="la-book">
+		<figure class='image is-3x3'>
+			<a href="https://www.amazon.com/dp/1737329506?ref_=pe_3052080_397514860"><img 
+				src='/home/LIsa-Anne-Just-released1-01.jpg' alt="The Secret Circle"></a>
+		</figure>
+	</div>
+	<!-- <div class='column is-half'>
+		<div class='card equal-height'>
+		  <div class='card-content is-flex is-horizontal-center'>
+			<figure class='image is-3x3'><img src='/home/LIsa-Anne-Just-released1-01.jpg'></figure>
+		  </div>
+		</div>
+	  </div> -->
 	
 </div>
 <style lang="scss">
@@ -16,5 +28,17 @@
 		width: 100%;
 	}
 }
+.la-book {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	figure {
+		width: 50%;
+		img {
+			width: 100%;
+		}
+	}
+	}
+
 
 </style>
